9f1ed3255d2a3b5dae08df699bb0a99ffe288727
Muhammad Agung Zuhdi <janissaryagung@gmail.com>
2022-10-24 16:03:25 +0700

    add new

---
+++ pages/users/detail.tsx
@@ -0,0 +1,5 @@
+export default function userDetail() {
+  return (
+    <div>userDetail</div>
+  )
+}
---
+++ pages/users/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function usersPage() {
+  return (
+    <div>usersPage</div>
+  )
+}
