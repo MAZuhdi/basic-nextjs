c8b6c4a64fd6e79d5b0b621d8f7084ae1c2d270b
Muhammad Agung Zuhdi <janissaryagung@gmail.com>
2022-10-24 16:49:27 +0700

    Auto redirection #11

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
 export default function Custom404() {
+
+  const router = useRouter();
+
+  
+  useEffect(() => {
+    setTimeout(() => {
+        // console.log("dipanggil setelah 5 detik")
+        router.push('/');
+    }, 5000);
+  }, []);
+
   return (
     <div>
         <h1 className="title-not-found">Ooopss...</h1>
