dfec49209712bc469879b79885b724b339d2cfbd
Muhammad Agung Zuhdi <janissaryagung@gmail.com>
2022-10-24 16:41:33 +0700

    Membuat halaman 404(not found page) Custom #10

---
+++ pages/404.tsx
@@ -0,0 +1,8 @@
+export default function Custom404() {
+  return (
+    <div>
+        <h1 className="title-not-found">Ooopss...</h1>
+        <h1 className="title-not-found">The page youre lookin for is not found</h1>
+    </div>
+  )
+}
